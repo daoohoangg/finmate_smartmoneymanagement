d8020a6ba199b715062d4bb11148047e67f055af
LamVH <lamgameplayforme@gmail.com>
2026-02-02 23:25:37 +0700

    Service layer 56

---
+++ finmate_smartmoneymanagement_be/src/main/java/com/finmate/repository/WalletRepository.java
@@ -0,0 +1,11 @@
+package com.finmate.repository;
+
+import com.finmate.entities.Wallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface WalletRepository extends JpaRepository<Wallet, Long> {
+    List<Wallet> findByUserId(UUID userId);
+}
