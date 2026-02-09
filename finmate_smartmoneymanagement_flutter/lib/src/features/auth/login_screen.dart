2cfda336115327509e4b2305b9734cdd2337b21c
Hoang Trung Dao <64149095+daoohoangg@users.noreply.github.com>
2026-02-09 22:42:52 +0700

    feature/add dockerfile and homescreen

--- finmate_smartmoneymanagement_flutter/lib/src/features/auth/login_screen.dart
+++ finmate_smartmoneymanagement_flutter/lib/src/features/auth/login_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../core/constants/app_colors.dart';
 import '../../core/storage/session_storage.dart';
-import '../settings/settings_screen.dart';
+import '../dashboard/monthly_dashboard_screen.dart';
 import 'services/auth_service.dart';
 import '../../shared/widgets/app_text_field.dart';
 import '../../shared/widgets/primary_button.dart';
@@ -49,7 +49,7 @@ class _LoginScreenState extends State<LoginScreen> {
         fullName: response.fullName,
       );
       if (!mounted) return;
-      Navigator.pushReplacementNamed(context, SettingsScreen.routeName);
+      Navigator.pushReplacementNamed(context, MonthlyDashboardScreen.routeName);
     } catch (e) {
       _showSnack(e.toString());
     } finally {
@@ -99,7 +99,7 @@ class _LoginScreenState extends State<LoginScreen> {
         fullName: response.fullName,
       );
       if (!mounted) return;
-      Navigator.pushReplacementNamed(context, SettingsScreen.routeName);
+      Navigator.pushReplacementNamed(context, MonthlyDashboardScreen.routeName);
     } catch (e) {
       _showSnack(e.toString());
     } finally {
